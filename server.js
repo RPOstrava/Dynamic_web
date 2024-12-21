067ecd572622a150ee33af787d9669d4828e0d3d
RPOstrava <warwarc097@gmail.com>
2024-12-21 18:31:04 +0100

    server.js updated

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require('cors'); // Import CORS
 const app = express();
 const port = 8000;
 
-let message = 'Initial message';
-
+app.use(cors()); // Povolit všechny domény (prozatím)
 app.use(express.static('public'));
 app.use(express.json());
+
+let message = 'Initial message';
 
 app.post('/change', (req, res) => {
     message = req.body.message;
